4afa4fc5b891ec1368f4894cb1ea84048cd94f8a
ebisLab <ebaiden@techie.com>
2019-08-13 19:26:20 -0400

    attempted to render list

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,14 +1,14 @@
 import React, { useState, useReducer } from 'react';
 import { initialState, Reducer } from '../reducers/Reducer';
 
-//actions - 
-//TOGGLE EDITING
-//
 const Todo = () => {
+    const [state, dispatch] = useReducer(Reducer, initialState) //reducer hook
+
 
   return (
     <div>
       <h2>Inside The Todo Component</h2>
+      {/* {dispatch({type:})} */}
     </div>
   );
 };
