ccb2c9b84945402892a7a26878733719f78852e1
ebisLab <ebaiden@techie.com>
2019-08-13 17:53:12 -0400

    step 1 finished

---
+++ todo/src/components/Todo.js
@@ -0,0 +1,16 @@
+import React, { useState, useReducer } from 'react';
+import { initialState, Reducer } from '../reducers/Reducer';
+
+//actions - 
+//TOGGLE EDITING
+//
+const Todo = () => {
+
+  return (
+    <div>
+      <h2>Inside The Todo Component</h2>
+    </div>
+  );
+};
+
+export default Todo;
